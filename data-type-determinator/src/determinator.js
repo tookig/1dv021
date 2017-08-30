172ad8188ef8fbf42237c5e945435538762cecac
Christian Olsson <christian.olsson@home.se>
2017-08-30 19:51:51 +0200

    function completed.

--- data-type-determinator/src/determinator.js
+++ data-type-determinator/src/determinator.js
@@ -16,10 +16,26 @@
  * @returns {string} The data type of the parameter.
  */
 function tellType (data) {
-  let result = 'You sent me '
-
-  // TODO: Write your code here.
-
+  let result = 'You sent me a'
+  if (data === null) {
+    result += ' null value.'
+  } else if (typeof data === 'undefined') {
+    result += 'n undefined value.'
+  } else if (typeof data === 'object') {
+    if (Array.isArray(data)) {
+      result += 'n array.'
+    } else {
+      result += 'n object.'
+    }
+  } else if (typeof data === 'boolean') {
+    result += ' boolean.'
+  } else if (typeof data === 'number') {
+    result += ' number.'
+  } else if (typeof data === 'string') {
+    result += ' string.'
+  } else if (typeof data === 'function') {
+    result += ' function.'
+  }
   return result
 }
 
