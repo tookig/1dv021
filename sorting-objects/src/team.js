6ac0e383c3e634ad19497f2a0b194c5fe8176c81
Christian Olsson <christian.olsson@home.se>
2017-08-30 20:36:56 +0200

    Sorting completed.

--- sorting-objects/src/team.js
+++ sorting-objects/src/team.js
@@ -16,7 +16,14 @@
    * @returns {Object[]} An ordered array of team objects.
    */
 function sortByPoints (teams) {
-  // TODO: Write your code here!
+  if (!Array.isArray(teams)) {
+    throw new TypeError('\'teams\' parameter must be an array')
+  }
+  var newTeams = teams.slice()
+  newTeams.sort(function sortTeams (team1, team2) {
+    return (team2.points || 0) - (team1.points || 0)
+  })
+  return newTeams
 }
 
 exports.sortByPoints = sortByPoints
