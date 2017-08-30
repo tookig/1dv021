7200022e6703551922f0e30b53217415adc8bace
Christian Olsson <christian.olsson@home.se>
2017-08-30 20:24:30 +0200

    Squashed 'sorted-array-copy/' content from commit 7ed9d98
    git-subtree-dir: sorted-array-copy
    git-subtree-split: 7ed9d98975486bb5e5d5dd2bfd312ae12f11a8b3

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Mats Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+const ma = require('./src/my-array.js')
+
+let originalArray = [2, 5, 1, 6]
+
+try {
+  console.log('Original array: ', originalArray)
+
+  let result = ma.immutableSort(originalArray) // should return [1, 2, 5, 6]
+  console.log('\nOriginal array: ', originalArray)
+  console.log('Sorted array: ', result)
+
+  result = ma.immutableSortDescending(originalArray) // should return [6, 5, 2, 1]
+  console.log('\nOriginal array: ', originalArray)
+  console.log('Sorted (descending) array: ', result)
+} catch (e) {
+  console.error(`An exception caught: ${e.message}`)
+}
+
+console.log()
+
+try {
+  ma.immutableSort() // should throw a TypeError
+  console.log('ma.immutableSort throws no TypeError!')
+} catch (e) {
+  console.error(`An exception caught: ${e.message}`)
+}
+
+try {
+  ma.immutableSortDescending() // should throw a TypeError
+  console.log('ma.immutableSortDescending throws no TypeError!')
+} catch (e) {
+  console.error(`An exception caught: ${e.message}`)
+}
