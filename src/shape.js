d80044a07833303be31121d1d007dcec05f5cf10
Christian Olsson <christian.olsson@home.se>
2017-08-30 19:32:40 +0200

    Squashed 'right-triangle/' content from commit 2e264b1
    git-subtree-dir: right-triangle
    git-subtree-split: 2e264b134ad20c5592cd51b1bf1e898806a19e4c

---
+++ src/shape.js
@@ -0,0 +1,21 @@
+/**
+ * Shape module.
+ *
+ * @module src/shape
+ * @author Mats Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+/**
+ * Returns a string representing a right triangle.
+ *
+ * @param {number} base The number of characters in the triangle's base.
+ * @returns {string} A string of hash characters.
+ */
+function createRightTriangle (base) {
+  // TODO: Write your code here.
+}
+
+exports.createRightTriangle = createRightTriangle
