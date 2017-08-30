87f53a9b0ef1dbb379fcd37290b3c962153ec8a7
Christian Olsson <christian.olsson@home.se>
2017-08-30 21:00:36 +0200

    Squashed 'reduced-array/' content from commit 1e68d77
    git-subtree-dir: reduced-array
    git-subtree-split: 1e68d77c0b3f2f9d826269df1845cbf5e5a6c1c7

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Mats Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+const ma = require('./src/my-array')
+
+let result = ma.getSum([1, 2, 3])
+
+console.log(`${result} [90m=> [32m6[39m`)
