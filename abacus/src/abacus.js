9894be7b0a0ae2bda99f92cd3ed2d3637bc3cae0
Christian Olsson <christian.olsson@home.se>
2017-09-04 11:06:58 +0200

    Can handle numbers and string

--- abacus/src/abacus.js
+++ abacus/src/abacus.js
@@ -15,9 +15,24 @@
  * @returns {Number} The sum of an arbitrary number of numbers.
  */
 function add (varArgs) {
-  // Utilize the 'arguments' object here, not the 'varArgs' parameter.
+  let sum = 0
+  for (let i in arguments) {
+    let val = arguments[i]
+    if (Array.isArray(val)) {
 
-  // TODO: Write your code here.
+    } else if (typeof val === 'string') {
+      let number = Number.parseFloat(val.replace(',', '.'))
+      if (Number.isNaN(number)) {
+        throw new TypeError('At least one of the arguments can\'t be parsed as a number.')
+      }
+      sum += number
+    } else if (typeof val === 'number') {
+      sum += val
+    } else {
+      throw new TypeError('At least one of the arguments can\'t be parsed as a number.')
+    }
+  }
+  return sum
 }
 
 exports.add = add
